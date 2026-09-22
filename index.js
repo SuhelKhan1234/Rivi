ec1a83adccbdf950b3b5af3ce2353e84e2689cec
suhel khan8080@gmail.com <suhailali8080@gmail.com>
2026-09-22 18:25:47 +0530

    Revision

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ const port = 3000;
 app.use(express.json());
 
 
+// get Request
+app.get("/", (req, res)=>{
+    res.send(`<h1>This is heading<h1>`)
+})
+
+
+app.post ("/car", (req,res)=>{
+    res.send("Received a post Request");
+})
+
 app.listen(port, ()=>{
-    console.log("App is Running")
+    console.log("App is Running.")
 })
